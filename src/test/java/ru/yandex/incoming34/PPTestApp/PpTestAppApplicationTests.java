3dce65d7cb45dd3f88e0d91bbaa8295e92d3cc57
Sergei Aidinov <incoming34@yandex.ru>
2022-10-07 12:53:29 +0300

    Swagger starts

---
+++ src/test/java/ru/yandex/incoming34/PPTestApp/PpTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.incoming34.PPTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
